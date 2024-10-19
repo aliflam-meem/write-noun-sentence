3254e4e723ed238bf6d8446f9c3e4def529b18c2
Sana-Fawal <36355842+Sana-Fawal@users.noreply.github.com>
2024-10-19 20:30:59 +0300

    LAST EDIT

--- jar_bingo/jar_bingo_settings.py
+++ jar_bingo/jar_bingo_settings.py
@@ -22,18 +22,19 @@ WIN_LENGTH = 3  # Adjust this value to change the required winning streak
 #paths
 ## images
 ### Screen,board,tiles,quiz_card
-BACKGROUND_IMG = "images/Gemini_Generated_Image_5ivjha5ivjha5ivj.jfif"
-QUIZ_IMG = "images/card_Gemini_Generated_Image_2jadgq2jadgq2jad.jfif"
-BLUE_JELLYFISH_TILE = "images/Gemini_Generated_Image_b7o9wwb7o9wwb7o9 (1).jfif" 
-GREEN_JELLYFISH_TILE = "images/Gemini_Generated_Image_3n68uq3n68uq3n68.jfif"
-RED_JELLYFISH_TILE = "images/Gemini_Generated_Image_501zw2501zw2501z.jfif"
+BACKGROUND_IMG = "jar_bingo/images/Gemini_Generated_Image_5ivjha5ivjha5ivj.jfif"
+QUIZ_IMG = "jar_bingo/images/card_Gemini_Generated_Image_2jadgq2jadgq2jad.jfif"
+BLUE_JELLYFISH_TILE = "jar_bingo/images/Gemini_Generated_Image_b7o9wwb7o9wwb7o9 (1).jfif" 
+GREEN_JELLYFISH_TILE = "jar_bingo/images/Gemini_Generated_Image_3n68uq3n68uq3n68.jfif"
+RED_JELLYFISH_TILE = "jar_bingo/images/Gemini_Generated_Image_501zw2501zw2501z.jfif"
 ### Game Over
-WIN_MENU_IMG = "images/win_Gemini_Generated_Image_rw3exvrw3exvrw3e.jfif"
-LOSE_MENU_IMG = "images/lose_Gemini_Generated_Image_8m9gt98m9gt98m9g.jfif"
+WIN_MENU_IMG = "jar_bingo/images/win_Gemini_Generated_Image_rw3exvrw3exvrw3e.jfif"
+LOSE_MENU_IMG = "jar_bingo/images/lose_Gemini_Generated_Image_8m9gt98m9gt98m9g.jfif"
 ## Audio
-BACKGROUND_SEA_SHP = "audio/mixkit-wooden-ship-on-the-sea-1187.wav"
+BACKGROUND_SEA_SHP = "jar_bingo/audio/mixkit-wooden-ship-on-the-sea-1187.wav"
+WIN_1 = "jar_bingo/audio/Fliki_you_win.mp3"
 ## Fonts
-F_Arial = "fonts/Arial.ttf"
+F_Arial = "jar_bingo/fonts/Arial.ttf"
 
 # colors
 WHITE = (255, 255, 255)
